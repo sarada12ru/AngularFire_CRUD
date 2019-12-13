3392cb8f974b0f26753c08cf0b022d5043d69e27
SARADA PRASAD BISWAL <sarada12ru@gmail.com>
2019-12-13 16:47:43 +0530

    Configure Angular Routing Progress Indicator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GstAddComponent } from './gst-add/gst-add.component';
 import { GstGetComponent } from './gst-get/gst-get.component';
 import { GstEditComponent } from './gst-edit/gst-edit.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { SlimLoadingBarModule } from "ng2-slim-loading-bar";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,10 @@ import { GstEditComponent } from './gst-edit/gst-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
